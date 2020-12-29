d70eb32413422155998d20a62f472277c0f65f2b
pomeadad <72838354+pomeadad@users.noreply.github.com>
2020-12-30 03:29:50 +0700

    ทำEntity User ของระบบย่อยA - close #4

--- backend/ent/schema/user.go
+++ backend/ent/schema/user.go
@@ -1,11 +1,7 @@
 package schema
 
-import (
-	"github.com/facebook/ent"
-	"github.com/facebook/ent/schema/field"
-	"github.com/facebook/ent/schema/edge"
+import "github.com/facebookincubator/ent"
 
-)
 // User holds the schema definition for the User entity.
 type User struct {
 	ent.Schema
@@ -13,16 +9,10 @@ type User struct {
 
 // Fields of the User.
 func (User) Fields() []ent.Field {
-	return []ent.Field{
-		field.String("name").NotEmpty(),
-		field.String("email").NotEmpty(),
-	}
+	return nil
 }
 
 // Edges of the User.
 func (User) Edges() []ent.Edge {
-	return []ent.Edge{
-		edge.To("playlists", Playlist.Type).StorageKey(edge.Column("owner_id")), 
-		edge.To("videos", Video.Type).StorageKey(edge.Column("owner_id")),
-	}
+	return nil
 }
